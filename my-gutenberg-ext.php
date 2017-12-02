030b8360688c51b9d97d3ef2fcacbbe2b77bd6c0
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-12-02 13:04:16 +0900

    init

---
+++ my-gutenberg-ext.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: my-gutenberg-ext
+Plugin URI: https://github.com/ryo-utsunomiya
+Description: Sample implementation of Gutenberg customize.
+Version: 0.1
+Author: Ryo Utsunomiya
+Author URI: https://ryo511.info
+License: GPLv2
+*/
+

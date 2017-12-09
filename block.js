15ec3756f44e724ee0fedd798b7b4be3f1f6d40a
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-12-09 21:00:31 +0900

    My First Custom Block Type!

---
+++ block.js
@@ -0,0 +1,12 @@
+/* global wp */
+console.log('block.js');
+const { registerBlockType } = wp.blocks;
+const blockStyle = { backgroundColor: '#900', color: '#fff', padding: '20px' };
+
+registerBlockType('my-gutenberg-ext/hello-world', {
+    title: 'Hello World',
+    icon: 'universal-access-alt',
+    category: 'layout',
+    edit: () => <p style={blockStyle}>Hello editor.</p>,
+    save: () => <p style={blockStyle}>Hello saved content.</p>,
+});
--- my-gutenberg-ext.php
+++ my-gutenberg-ext.php
@@ -9,3 +9,13 @@ Author URI: https://ryo511.info
 License: GPLv2
 */
 
+function my_gutenberg_ext_enqueue_block_editor_assets()
+{
+    wp_enqueue_script(
+        'my-gutenberg-ext',
+        plugins_url('block.build.js', __FILE__),
+        array('wp-blocks', 'wp-element')
+    );
+}
+
+add_action('enqueue_block_editor_assets', 'my_gutenberg_ext_enqueue_block_editor_assets');
